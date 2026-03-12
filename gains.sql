d49bc60cc83210d20d03c30169a99da37880db43
Fishmarchande <masonneon@gmail.com>
2026-03-12 10:28:28 -0600

    finished?

--- gains.sql
+++ gains.sql
@@ -1,50 +1,60 @@
 
 CREATE DATABASE gains;
 
+DROP TABLE IF EXISTS "user";
 CREATE TABLE "user" (
 	"name" character varying(15),
 	"surname" character varying(15),
     "age" smallint,
     "weight" smallint,
     "gender" char,
-	"userid" character(5) NOT NULL PRIMARY KEY
+	"userid" smallint NOT NULL PRIMARY KEY
 );
 
+DROP TABLE IF EXISTS "routine";
 CREATE TABLE "routine" (
 	"name" character varying(15),
 	"type" character varying(15),
-    "routineid" character(5) NOT NULL PRIMARY KEY,
-    FOREIGN KEY "user"
-    REFERENCES user(userid)
+    "routineid" smallint NOT NULL PRIMARY KEY,
+    "userid" smallint NOT NULL,
+    FOREIGN KEY ("userid") REFERENCES "user"("userid")
 );
 
+DROP TABLE IF EXISTS "workout";
 CREATE TABLE "workout" (
 	"name" character varying(15),
-	"workoutid" character(5) NOT NULL PRIMARY KEY,
-    FOREIGN KEY "user"
-    REFERENCES user(userid),
-    FOREIGN KEY "routine"
-    REFERENCES routine(routineid)
+	"workoutid" smallint NOT NULL PRIMARY KEY,
+    "routineid" smallint NOT NULL,
+    "userid" smallint NOT NULL,
+    FOREIGN KEY ("userid") REFERENCES "user"("userid"),
+    FOREIGN KEY ("routineid") REFERENCES "routine"("routineid")
 );
 
---TODO need a linking table workout -> exercise since this is many to many 
-
+DROP TABLE IF EXISTS "exercise";
 CREATE TABLE "exercise" (
 	"name" character varying(15),
     "muscle" character varying(15),
-	"exerciseid" character(5) NOT NULL PRIMARY KEY,
+	"exerciseid" smallint NOT NULL PRIMARY KEY
 );
 
+DROP TABLE IF EXISTS "workout_exercise";
+CREATE TABLE "workout_exercise" (
+	"workoutid" smallint NOT NULL,
+	"exerciseid" smallint NOT NULL,
+    FOREIGN KEY ("workoutid") REFERENCES "user"("userid"),
+    FOREIGN KEY ("exerciseid") REFERENCES "routine"("routineid")
+
+);
+
+DROP TABLE IF EXISTS "set";
 CREATE TABLE "set" (
     "set_id" serial PRIMARY KEY,
     "reps" smallint,
     "weight" smallint,
-    CONSTRAINT
-    FOREIGN KEY "exercise"
-    REFERENCES exercise(exerciseid)
-    CONSTRAINT
-    FOREIGN KEY "workout"
-    REFERENCES workout(workoutid)
+    "exerciseid" smallint NOT NULL,
+    "workoutid" smallint NOT NULL,
+    FOREIGN KEY ("exerciseid") REFERENCES "exercise"("exerciseid"),
+    FOREIGN KEY ("workoutid") REFERENCES "workout"("workoutid")
 );
 
 --TODO 
