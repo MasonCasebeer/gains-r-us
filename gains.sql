cbde5cbb0c552b2877c2adf0b25fc11f21e7e014
Fishmarchande <masonneon@gmail.com>
2026-03-12 08:42:32 -0600

    empty tables

--- gains.sql
+++ gains.sql
@@ -1,2 +1,22 @@
 
 CREATE DATABASE gains;
+
+CREATE TABLE "user" (
+	"name" character varying(15),
+	"userid" character(5) NOT NULL,
+);
+
+CREATE TABLE "routine" (
+	"name" character varying(15),
+	"routineid" character(5) NOT NULL,
+);
+
+CREATE TABLE "workout" (
+	"name" character varying(15),
+	"workoutid" character(5) NOT NULL,
+);
+
+CREATE TABLE "exercise" (
+	"name" character varying(15),
+	"exerciseid" character(5) NOT NULL,
+);
